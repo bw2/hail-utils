653c99e0c21932c9847aba106f40758cd0c01fc4
bw2 <bw2@users.noreply.github.com>
2019-07-14 13:08:33 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# based on https://uoftcoders.github.io/studyGroup/lessons/python/packages/lesson/
+
+from setuptools import setup
+
+setup(
+    name='hail_utils',
+    url='https://github.com/bw2/hail_utils',
+    author='Ben',
+    author_email='ben.weisburd@gmail.com',
+    packages=['hail_utils'],
+    install_requires=['hail'],
+    version='0.1',
+    license='MIT',
+    description='Misc. hail utils',
+)
