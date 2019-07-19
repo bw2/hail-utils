abaf87168b3b9a41bf3bee60ada21875dd96ce9f
bw2 <weisburd@broadinstitute.org>
2019-07-19 12:43:29 -0400

    testing spark install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools.command.install import install
 import os
 import urllib.request
-import sys
+
 # from https://mvnrepository.com/artifact/com.google.cloud.bigdataoss/gcs-connector/hadoop2-1.9.17
 
 GCS_CONNECTOR_URL = 'https://repo1.maven.org/maven2/com/google/cloud/bigdataoss/gcs-connector/hadoop2-1.9.17/gcs-connector-hadoop2-1.9.17.jar'
@@ -14,14 +14,22 @@ GCS_CONNECTOR_URL = 'https://repo1.maven.org/maven2/com/google/cloud/bigdataoss/
 class PostInstallCommand(install):
 
     def run(self):
-        self.announce(self.install_base, level=3)
-        raise ValueError(str(self.root) + "\n")
 
-        os.path.join(self.install_base,)
+        for root, dirs, files in os.walk(self.install_base):
+            pyspark_jars_dir = [d for d in dirs if "pyspark/jars" in d]
+            if pyspark_jars_dir:
+                pyspark_jars_dir = pyspark_jars_dir[0]
+                break
+        else:
+            self.warn("pyspark not found in " + str(self.install_base) + ". Unable to install GCS connector.")
+
+        self.announce(self.install_base, level=3)
+
+        local_jar_path = os.path.join(self.install_base, pyspark_jars_dir, os.path.basename(GCS_CONNECTOR_URL))
         try:
-            urllib.request.urlretrieve(GCS_CONNECTOR_URL, '/usr/local/lib/python3.7/site-packages/pyspark/jars/gcs-connector-hadoop2-latest.jar')
+            urllib.request.urlretrieve(GCS_CONNECTOR_URL, local_jar_path)
         except Exception as e:
-            self.warn('Unable to download GCS connector: ' + str(e))
+            self.warn("Unable to download GCS connector. " + str(e))
 
         install.run(self)
 
