21a38e8722b602ff9eeb2151cbc10bbe196e7f5d
bw2 <weisburd@broadinstitute.org>
2019-07-19 12:04:34 -0400

    testing spark install

--- setup.py
+++ setup.py
@@ -2,6 +2,34 @@
 
 from setuptools import setup
 
+from distutils.command.sdist import sdist as sdist_orig
+from distutils.errors import DistutilsExecError
+import os
+import urllib.request
+
+# from https://mvnrepository.com/artifact/com.google.cloud.bigdataoss/gcs-connector/hadoop2-1.9.17
+
+GCS_CONNECTOR_URL = 'https://repo1.maven.org/maven2/com/google/cloud/bigdataoss/gcs-connector/hadoop2-1.9.17/gcs-connector-hadoop2-1.9.17.jar'
+
+
+class sdist(sdist_orig):
+
+    def run(self):
+        print("DIRECTORY:")
+        print(os.getcwd())
+        print(os.path.abspath(os.getcwd()))
+
+        try:
+            urllib.request.urlretrieve(GCS_CONNECTOR_URL, '/usr/local/lib/python3.7/site-packages/pyspark/jars/gcs-connector-hadoop2-latest.jar')
+        except Exception as e:
+            self.warn('Unable to download GCS connector: ' + str(e))
+
+        #try:
+        #    self.spawn(['ls', '-l'], shell=True)
+        #except DistutilsExecError:
+        #    self.warn('listing directory failed')
+        super().run()
+
 setup(
     name='hail_utils',
     url='https://github.com/bw2/hail-utils',
@@ -12,4 +40,7 @@ setup(
     version='0.1',
     license='MIT',
     description='Misc. hail utils',
+    cmdclass={
+        'sdist': sdist
+    }
 )
