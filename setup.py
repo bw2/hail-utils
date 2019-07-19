a7b75cc4b5c44ee2d1ebde7c93bbd29edb3010ef
bw2 <weisburd@broadinstitute.org>
2019-07-19 12:19:17 -0400

    testing spark install

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ GCS_CONNECTOR_URL = 'https://repo1.maven.org/maven2/com/google/cloud/bigdataoss/
 class PostInstallCommand(install):
 
     def run(self):
-        raise ValueError("DIRECTORY:" + "\n")
-        raise ValueError(os.getcwd() + "\n")
         raise ValueError(os.path.abspath(os.getcwd()) + "\n")
         raise ValueError(str(self.build_lib) + "\n")
         raise ValueError(str(self.install_base) + "\n")
